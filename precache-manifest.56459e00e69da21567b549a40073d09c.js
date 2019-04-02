eff622a13122b13bb716bb18de7add6dd054b484
Laura Gillespie <lagillespie10@gmail.com>
2019-04-02 19:11:33 -0400

    Updates

---
+++ precache-manifest.56459e00e69da21567b549a40073d09c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4716bd675575152abf1d",
+    "url": "/lagilles/clickyGame/static/css/main.0719d05b.chunk.css"
+  },
+  {
+    "revision": "4716bd675575152abf1d",
+    "url": "/lagilles/clickyGame/static/js/main.92f4f36b.chunk.js"
+  },
+  {
+    "revision": "f9c996a627103f506653",
+    "url": "/lagilles/clickyGame/static/js/runtime~main.95b59af5.js"
+  },
+  {
+    "revision": "85f1719d56c8f7d51e2c",
+    "url": "/lagilles/clickyGame/static/js/2.278c4d8b.chunk.js"
+  },
+  {
+    "revision": "409fb088685c6c78685f22a1e867fde3",
+    "url": "/lagilles/clickyGame/static/media/background.409fb088.jpg"
+  },
+  {
+    "revision": "df2d605fda075420870a731742a5910e",
+    "url": "/lagilles/clickyGame/index.html"
+  }
+];
